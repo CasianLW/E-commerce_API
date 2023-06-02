cb05680a51444d6e53c85fda0af87d73e17c9142
CasianLW <ciorbacasian@gmail.com>
2023-06-02 12:13:59 +0200

    added subscriptions & imports routes

--- router/AdminRouter.js
+++ router/AdminRouter.js
@@ -13,7 +13,18 @@ const {
   getEventsList,
   getEvent,
 } = require("../controller/admin/adminEvents-controller");
-const { createGame } = require("../controller/admin/adminShop-controller");
+const {
+  createGame,
+  deleteGame,
+  editGame,
+  getGame,
+  listAllGames,
+  createSubscription,
+  deleteSubscription,
+  editSubscription,
+  getSubscription,
+  listAllSubscriptions,
+} = require("../controller/admin/adminShop-controller");
 
 exports.router = (() => {
   const adminRouter = express.Router();
@@ -33,11 +44,18 @@ exports.router = (() => {
   adminRouter.route("/events/:id").delete(deleteEvent);
 
   //   Games/products routes
-  //   adminRouter.route("/games/").get(getEventsList);
+  adminRouter.route("/games/").get(listAllGames);
   adminRouter.route("/games/").post(createGame);
-  //   adminRouter.route("/games/:id").get(getEvent);
-  //   adminRouter.route("/games/:id").put(editEvent);
-  //   adminRouter.route("/games/:id").delete(deleteEvent);
+  adminRouter.route("/games/:id").get(getGame);
+  adminRouter.route("/games/:id").put(editGame);
+  adminRouter.route("/games/:id").delete(deleteGame);
+
+  //   Subscriptions routes
+  adminRouter.route("/subscriptions/").get(listAllSubscriptions);
+  adminRouter.route("/subscriptions/").post(createSubscription);
+  adminRouter.route("/subscriptions/:id").get(getSubscription);
+  adminRouter.route("/subscriptions/:id").put(editSubscription);
+  adminRouter.route("/subscriptions/:id").delete(deleteSubscription);
   // adminRouter.route("/register/confirm/:token").get(confirmAccount);
   // authRouter.route("/home");
   // authRouter.route("/admin/users/:token").get(getUsers);
